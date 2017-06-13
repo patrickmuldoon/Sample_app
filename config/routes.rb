11e1d53be578456f49205157f43a14b8e450324e
Patrick Muldoon <patrick_muldoon@Patricks-MacBook-Pro.local>
2017-06-13 00:23:23 -0400

    Implement basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'users/new'
 
   root 'static_pages#home'
@@ -13,7 +15,11 @@ Rails.application.routes.draw do
 
   get '/signup', to: 'users#new'
 
-  post '/signup', to: 'users#create'
+  get '/login', to: 'sessions#new'
+
+  post '/login', to: 'sessions#create'
+
+  delete '/logout', to: 'sessions#destroy'
 
   resources :users
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -13,7 +13,20 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                         password:  "password",
                                         password_confirmation: "password" }}
     end
-      follow_redirect!
-      assert_template 'users/show'
+    follow_redirect!
+    assert_template 'users/show'
+  end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  "Example User",
+                                         email: "user@example.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert is_logged_in?
   end
 end
